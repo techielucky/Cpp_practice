b658cc0483b2a2a329406ca86f26872a89a5448f
Lucky Khan <djalok9507@gmail.com>
2026-09-23 22:29:35 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 void main{
   cout<<"hello world";
-  if 
-    else()
+ if (number > 0) {
+        cout << "The number is positive." << endl;
+    } else {
+        cout << "The number is not positive." << endl;
+    }
     return 0;
 }
