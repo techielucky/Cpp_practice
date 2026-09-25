cc0102c0fa81e821d03efcd27433f24e9b467027
Lucky Khan <djalok9507@gmail.com>
2026-09-26 00:14:17 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void main{
   cout<<"hello world....!";
  if (number > 0) {
-        cout << "The number is positive." << endl;
+        cout << "The number is positive.." << endl;
     } else {
         cout << "The number is not positive." << endl;
     }
